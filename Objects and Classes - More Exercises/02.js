f7bf2fe669dddb2cfd1de471b05d5152e6f30448
SpasMihaylov <78800299+Spas52@users.noreply.github.com>
2023-11-18 22:41:03 +0200

    Add files via upload

---
+++ Objects and Classes - More Exercises/02.js
@@ -0,0 +1,61 @@
+function catalog(products) {
+    // Sort products alphabetically
+    products.sort(function(a, b) {
+        // Extract product names for comparison (case-insensitive)
+        var productNameA = a.split(' : ')[0].toUpperCase();
+        var productNameB = b.split(' : ')[0].toUpperCase();
+
+        // Compare product names
+        if (productNameA < productNameB) {
+            return -1;
+        } else if (productNameA > productNameB) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    // Initialize an empty object to store products by initial
+    var productGroups = {};
+
+    // Group products by initial
+    products.forEach(function(product) {
+        var productName = product.split(' : ')[0];
+        var initial = productName[0].toUpperCase();
+
+        if (!productGroups[initial]) {
+            productGroups[initial] = [];
+        }
+
+        productGroups[initial].push(product);
+    });
+
+    // Print the catalog
+    for (var initial in productGroups) {
+        console.log(initial);
+        productGroups[initial].forEach(function(product) {
+            var [productName, productPrice] = product.split(' : ');
+            console.log(`  ${productName}: ${productPrice}`);
+        });
+    }
+}
+
+catalog([
+
+    'Appricot : 20.4',
+    
+    'Fridge : 1500',
+    
+    'TV : 1499',
+    
+    'Deodorant : 10',
+    
+    'Boiler : 300',
+    
+    'Apple : 1.25',
+    
+    'Anti-Bug Spray : 15',
+    
+    'T-Shirt : 10'
+    
+    ] )
